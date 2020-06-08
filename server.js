a678ab98f20ebe5684a287739f831c066e9dc4bd
lopezsarmiento <diego.lopezsarmiento@gmail.com>
2020-06-08 01:11:11 -0300

    Adding backend functionality

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv"); // allows to create global vars
+const colors = require("colors"); // colors for console
+const morgan = require("morgan"); // logging
+const connectDB = require("./config/db");
+const transactions = require("./routes/transactions");
+
+dotenv.config({ path: "./config/config.env" });
+
+// start DB connection
+connectDB();
+
+const app = express();
+
+// this allows us to use bodyparser
+app.use(express.json());
+
+// api route
+app.use("/api/v1/transactions", transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
